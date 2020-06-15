693e8d0fd739de3652386800f432e5021223ee48
George Savchenko <george.savchenko@gmail.com>
2020-06-14 20:47:22 -0400

    importing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NativeScriptModule } from "@nativescript/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { HomeComponent } from "./home";
+import { ContactComponent } from "./contact";
+import { AboutComponent } from './about';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -22,8 +23,9 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        HomeComponent,
+        ContactComponent,
+        AboutComponent
     ],
     providers: [],
     schemas: [
