d6bc868310408d372b4fac36ae2b9eddeb4542d2
George Savchenko <george.savchenko@gmail.com>
2020-06-14 20:43:04 -0400

    adding page components

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "Contact",
+  templateUrl: "contact.component.html",
+  styleUrls: ["contact.component.css"]
+})
+export class ContactComponent {
+    constructor() { }
+}
