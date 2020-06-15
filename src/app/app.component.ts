ab546c2adca1d01eac968f823e551b581c4071d0
George Savchenko <george.savchenko@gmail.com>
2020-06-14 20:47:09 -0400

    adding navigation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,17 @@
 import { Component } from "@angular/core";
+import { RouterExtensions } from "@nativescript/angular/router";
 
 @Component({
     selector: "ns-app",
     templateUrl: "./app.component.html"
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(private routerExtensions: RouterExtensions) {}
+
+  public navigate(link: string): void {
+    this.routerExtensions.navigate([link], {
+      animated: true,
+      transition: { name: 'slide' }
+    });
+  }
+}
